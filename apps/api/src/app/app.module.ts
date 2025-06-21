73cb53d7e735b70993c4f11c219ba435b8ec64c9
Maher Alhamod <ma8h7er@gmail.com>
2025-06-21 11:34:01 -0400

    init home resource with configs

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { HomeModule } from './home/home.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [],
+      envFilePath: ['.env'],
+    }),
+    HomeModule,
+  ],
 })
 export class AppModule {}
