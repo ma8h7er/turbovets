1c241ad33746df2f061bed39e5ea8358c396b488
Maher Alhamod <ma8h7er@gmail.com>
2025-06-21 11:44:16 -0400

    add utils and update main.ts file

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { HomeModule } from './home/home.module';
+import appConfig from './config/app.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [],
+      load: [appConfig],
       envFilePath: ['.env'],
     }),
     HomeModule,
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,19 +1,36 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { useContainer } from 'class-validator';
+import { ConfigService } from '@nestjs/config';
+import { AllConfigType } from './app/config/config.type';
+import validationOptions from './app/utils/validation-options';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  const app = await NestFactory.create(AppModule, { cors: true });
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+  const configService = app.get(ConfigService<AllConfigType>);
+
+  app.enableShutdownHooks();
+
+  const globalPrefix = configService.getOrThrow('app.apiPrefix', {
+    infer: true,
+  });
+
+  app.setGlobalPrefix(globalPrefix, {
+    exclude: ['/'],
+  });
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  app.useGlobalPipes(new ValidationPipe(validationOptions));
+
+  const port = configService.getOrThrow('app.port', { infer: true });
   await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
 }
 
 bootstrap();
